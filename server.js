cbe12a0e2002851ba2155943b8074bc497fbeb33
Magali Dugué <77433765+magalidugue@users.noreply.github.com>
2022-05-07 15:36:13 +0200

    server config

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
-const express= require ('express')
-require('dotenv').config({path: './config/.env'})
-require('./config/db.js')
-const app = express()
+const bodyParser = require("body-parser");
+const express = require("express");
+const morgan = require("morgan");
+require("dotenv").config({ path: "./config/.env" });
+require("./config/db.js");
+const app = express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(morgan("tiny"));
+app.use(express.json());
 
 app.listen(process.env.PORT, () => {
-    console.log(`listening on port ${process.env.PORT}`)
-})
+  console.log(`listening on port ${process.env.PORT}`);
+});
